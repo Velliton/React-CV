3c687623cd849ba9795b0d053158b7226895eb17
Velliton <evg92faut@gmail.com>
2022-09-11 00:35:33 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import './App.css';
+import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="header">
+        <nav className='nav container'>
+          <a>ХЗ</a>
+          <ul>
+            <a href='#'>
+              <li>
+                <i class="uil uil-estate nav__icon"></i>главная
+                </li>
+            </a>
+            <a href='#'>
+              <li>
+                <i class="uil uil-estate nav__icon"></i>главная
+                </li>
+            </a>
+            <a href='#'>
+              <li>
+                <i class="uil uil-estate nav__icon"></i>главная
+                </li>
+            </a>
+            <a href='#'>
+              <li>
+                <i class="uil uil-estate nav__icon"></i>главная
+                </li>
+            </a>
+            <a href='#'> 
+              <li>
+                <i class="uil uil-estate nav__icon"></i>главная
+                </li>
+            </a>
+            <a href='#'>
+              <li>
+                <i class="uil uil-estate nav__icon"></i>главная
+                </li>
+            </a>
+            
+          </ul>
+          <i class="uil uil-times nav__close" id="nav-close"></i>
+        </nav>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
-          rel="noopener noreferrer"
+          rel=""
         >
-          Learn React
+          
         </a>
       </header>
     </div>
